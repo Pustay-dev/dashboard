d6b81a7ebb8fa1e34da159ce326544627dec6301
Pustay-dev <pustay.jakub@gmail.com>
2026-09-03 11:51:57 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,12 +1,8 @@
 // Configure these values with your Supabase project credentials.
 // DO NOT commit real secrets to public repos if this repo is public.
 window.SUPABASE_CONFIG = {
-  url: '
-
-', // e.g. 'https://xyzabc.supabase.co'
-  anonKey: '
-
-' // e.g. 'public-anon-key'
+  url: 'https://dipchrxyyfzuwucstsuc.supabase.co', // e.g. 'https://xyzabc.supabase.co'
+  anonKey: 'sb_publishable_uFcqbB9TKyGtYdLywsfd5w_6fyzOECc' // e.g. 'public-anon-key'
 };
 
 // If the Supabase lib is loaded, initialize a client for convenience.
